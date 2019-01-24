98656f6d25628e34924db14307b5786f2870e6e6
Novaedra <leofouquier20111999@gmail.com>
2019-01-24 16:10:24 +0100

    tout

--- application/views/include/following_menu.php
+++ application/views/include/following_menu.php
@@ -1,5 +1,5 @@
 <!-- Following Menu -->
-<div class="ui large top fixed hidden menu">
+<div class="ui large top fixed hidden menu inverted">
     <div class="ui container">
         <?= anchor('', 'Accueil', 'class = "active item"'); ?>
         <a class="item" href="#CV">CV</a>
@@ -8,10 +8,10 @@
         <a class="item" href="#accessibilité">Accessibilité</a>
         <div class="right menu">
             <div class="item">
-                <?= anchor('Accueil/login', 'Login', 'class = "ui button"'); ?>
+                <?= anchor('Accueil/login', 'Connexion', 'class = "ui inverted button"'); ?>
             </div>
             <div class="item">
-                <?= anchor('Accueil/inscription', 'Inscription', 'class = "ui primary button"'); ?>
+                <?= anchor('Accueil/inscription', 'Inscription', 'class = "ui inverted button"'); ?>
             </div>
         </div>
     </div>
