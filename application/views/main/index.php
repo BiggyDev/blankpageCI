dfc9e7f41a58417c29bc270876c9388d88b77feb
Novaedra <leofouquier20111999@gmail.com>
2019-01-24 10:51:33 +0100

    gitignore

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -7,9 +7,10 @@
                         <i class="sidebar icon"></i>
                     </a>
                     <?= anchor('', 'Accueil', 'class = "active item"'); ?>
-                    <a class="item">Diplômes/formations</a>
-                    <a class="item">Expériences</a>
-                    <a class="item">Compétences</a>
+                    <a class="item" href="#CV">CV</a>
+                    <a class="item" href="#visibilité">Visibilité</a>
+                    <a class="item" href="#métier">Métier</a>
+                    <a class="item" href="#accessibilité">Accessibilité</a>
                     <div class="right item">
                         <?= anchor('Accueil/login', 'Connexion', 'class = "ui inverted button"'); ?>
                         <?= anchor('Accueil/inscription', 'Inscription', 'class = "ui inverted button"'); ?>
@@ -27,44 +28,35 @@
 
         </div>
 
-        <div class="ui piled segment">
-            <h2 class="ui header">Élaborez votre CV</h2><hr/>
-            <h3 class="sub header">Grâce aux outils fournis</h3>
-            <p>En renseignant quelques informations tel que vos études, vos compétences et vos passions vous pourrez choisir un design de CV et le mettre en ligne sur notre site WEB</p>
-        </div>
+<div class="ui text container" id="CV">
+    <h2>Élaborez votre CV</h2><hr/>
+    <h3>Grâce aux outils fournis</h3>
+    <p>En renseignant quelques informations tel que vos études, vos compétences et vos passions vous pourrez choisir un design de CV et le mettre en ligne sur notre site WEB</p>
+</div>
 
-        <div class="ui piled segment">
-            <h2 class="ui header">Augmenter votre visibilité</h2><hr/>
-            <h3 class="sub header">Auprès des professionnels</h3>
-            <p>Votre CV est visible auprès de tous les professionnels utilisant notre plateforme, dans ce cas n'hésitez pas à renseigner toutes vos compétences et autres informations sur vous dans votre CV</p>
-        </div>
+        <div class="ui hidden divider"></div>
 
-        <div class="ui piled segment">
-            <h2 class="ui header">Trouver le métier qui vous convient</h2><hr/>
-            <h3 class="sub header">Rencontrez des professionnels.</h3>
-            <p>Avec vos informations, nous sommes apte à vous mettre en relations avec des professionnels recherchant votre profil.</p>
-        </div>
+<div class="ui text container" id="visibilité">
+    <h2>Augmenter votre visibilité</h2><hr/>
+    <h3>Auprès des professionnels</h3>
+    <p>Votre CV est visible auprès de tous les professionnels utilisant notre plateforme, dans ce cas n'hésitez pas à renseigner toutes vos compétences et autres informations sur vous dans votre CV</p>
+</div>
 
-        <div class="ui piled segment">
-            <h2 class="ui header">Accessible partout</h2><hr/>
-            <h3 class="sub header">Sur toutes les plateformes</h3>
-            <p>En dehors des entretients vous pouvez avoir une occasions de donner votre CV en main propre ou simplement un moyen de l'afficher, c'est une part a ne pas négliger dans la recherche d'un emploi et elle fait souvent la différence</p>
-        </div>
+        <div class="ui hidden divider"></div>
 
-<div class="ui text container">
+<div class="ui text container" id="métier">
     <h2>Trouver le métier qui vous convient</h2><hr/>
     <h3>Rencontrez des professionnels.</h3>
     <p>Avec vos informations, nous sommes apte à vous mettre en relations avec des professionnels recherchant votre profil.</p>
 </div>
 
-<div class="ui divider"></div>
+        <div class="ui hidden divider"></div>
 
-<div class="ui text container">
+<div class="ui text container" id="accessibilité">
     <h2>Accessible partout</h2><hr/>
     <h3>Sur toutes les plateformes</h3>
     <p>En dehors des entretients vous pouvez avoir une occasions de donner votre CV en main propre ou simplement un moyen de l'afficher, c'est une part a ne pas négliger dans la recherche d'un emploi et elle fait souvent la différence</p>
 </div>
-
 
 <!-- Menu fondu -->
 <script>
